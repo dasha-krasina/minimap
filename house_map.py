88b6fb0643de9c2a1d892dc86fd4dcb3a578be01
Dasha Krasina <0x10dk@gmail.com>
2019-11-29 12:59:19 -0800

    Draw house map using Python Turtle.

---
+++ house_map.py
@@ -0,0 +1,94 @@
+import turtle #using turtle
+
+bob = turtle.Turtle()
+bob.forward(600)#making house outline
+bob.left(90)
+bob.forward(500)#houe outline
+bob.left(90)
+bob.forward(600)#house outline
+bob.left(90)
+bob.forward(500)#making house outline
+bob.left(90)
+bob.forward(100)#making alena's room
+bob.left(90)
+bob.forward(150)#making Alena's room
+bob.left(90)
+bob.forward(100)#making Alena's room
+bob.right(90)
+bob.forward(50)#making small room
+bob.right(90)
+bob.forward(100)#making small room
+bob.right(90)
+bob.forward(50)#making small room
+bob.left(180)
+bob.forward(350)#making the enterence
+bob.left(180)
+bob.forward(200)#coming to make my room
+bob.left(90)
+bob.forward(200)#making my room
+bob.left(90)
+bob.forward(200)#making my room
+bob.right(90)
+bob.forward(75)#coming to make left restroom
+bob.right(90)
+bob.forward(100)#making restroom
+bob.right(90)
+bob.forward(75)#making restroom
+bob.left(90)
+bob.forward(25)#coming to make heater
+bob.left(90)
+bob.forward(100)#making heater
+bob.left(90)
+bob.forward(25)#making heater
+bob.left(90)
+bob.forward(25)#finishing heater
+bob.left(180)
+bob.forward(25)#turning around
+bob.right(90)
+bob.forward(25)#coming to make right restroom
+bob.left(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(125)#finishing bathroom
+bob.left(180)
+bob.forward(125)
+bob.left(90)
+bob.forward(150)
+bob.left(180)
+bob.forward(25)#making blanket campartment
+bob.left(90)
+bob.forward(50)#making blanket compartment
+bob.left(90)
+bob.forward(25)#making blanket compartment
+bob.left(180)
+bob.forward(150)#making parent sleeping room
+bob.left(90)
+bob.forward(200)#making parent sleeping room
+bob.left(90)
+bob.forward(150)#completing parents sleeping room
+bob.left(180)
+bob.forward(150)#coming to make small room
+bob.left(90)
+bob.forward(125)#making small room
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
