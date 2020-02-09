c9376c7df141527b9e99887c25f13e6cceef156e
dasha-krasina <58310238+dasha-krasina@users.noreply.github.com>
2020-02-08 18:59:17 -0800

    Saving Multiple pics from ESP32
    Saving multiple images from ESP32 camera to python 3

---
+++ saving_multiple_pics.py
@@ -0,0 +1,15 @@
+import urllib3
+http = urllib3.PoolManager()
+
+for i in range(0, 5):
+    print("saving image #: " + str(i))
+    r = http.request('GET', "http://192.168.86.20/jpg", preload_content=False)
+    filename = "image-" + str(i) + ".jpg"
+    with open(filename, 'wb') as out:
+        while True:
+            data = r.read(1024)
+            if not data:
+                break
+            out.write(data)
+
+    r.release_conn()
